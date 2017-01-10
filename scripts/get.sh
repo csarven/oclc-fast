7b9c652d4fce6a551528482a20be57f24bb980e3
Sarven Capadisli <info@csarven.ca>
2017-01-10 15:16:31 +0100

    Script to retrieve and store FAST data

---
+++ scripts/get.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#http://experimental.worldcat.org/fast/download/FASTAll.nt.zip
+#data.txt contains individual URLs which happen to be more than what's in FASTAll.nt.zip
+
+root=$(dirname `pwd`)
+pathToData="$root"/data;
+pathToZip="$pathToData"/zip;
+
+mkdir -p "$pathToZip";
+
+while read url; do
+  wget "$url" -P "$pathToZip"/
+done < data.txt
